8393d83214ffbe3623d5baa7a5d14738bf299ab9
Maxim <vsmaxim@me.com>
2021-06-10 11:30:07 +0200

    Cachetime fix

--- src/hooks/api/useMatchPredictions.js
+++ src/hooks/api/useMatchPredictions.js
@@ -1,17 +1,21 @@
-import { useContext } from "react";
 import { useQuery } from "react-query";
-import { MainContext } from "../../App";
 import { db, querySnapshotToData } from "../../firebase";
 import useUser from "../useUser";
 
 const useMatchPredictions = () => {
   const user = useUser();
-  const { setIsLoading } = useContext(MainContext);
 
-  const { data, isLoading, error } = useQuery("predictions", async () =>
-    querySnapshotToData(
-      await db.collection("users").doc(user.uid).collection("predictions").get()
-    )
+  const { data, isLoading, error } = useQuery(
+    "predictions",
+    async () =>
+      querySnapshotToData(
+        await db
+          .collection("users")
+          .doc(user.uid)
+          .collection("predictions")
+          .get()
+      ),
+    { cacheTime: 1 }
   );
 
   return { predictions: data, isLoading, error };
