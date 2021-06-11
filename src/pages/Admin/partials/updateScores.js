0565f9273ff5a2b01d7d68beb373062b09d1e15b
Maxim <vsmaxim@me.com>
2021-06-11 23:39:14 +0200

    Points push

--- src/pages/Admin/partials/updateScores.js
+++ src/pages/Admin/partials/updateScores.js
@@ -14,41 +14,45 @@ export const updateScores = async () => {
         .collection("users")
         .doc(u.id)
         .collection("predictions")
-        .where("points", ">", -1)
+        .where("points", "==", -1)
         .get()
     );
 
     predictions?.forEach((prediction) => {
       let p = 0;
       if (!prediction.id.includes("question")) {
-        const outcomeHome = matches[prediction.id]?.outcomeHome;
-        const outcomeAway = matches[prediction.id]?.outcomeAway;
+        const match = matches.find((m) => m.id === prediction.id);
+        const outcomeHome = match?.outcomeHome;
+        const outcomeAway = match?.outcomeAway;
         const predHome = prediction.outcomeHome;
         const predAway = prediction.outcomeAway;
 
         // juise TOTO = 3 punten
         // 1 punt voor juiste aantal doelpunten (per team)
         // 2 bonuspunten indien match volledig juist -> 7 in totaal
-        if (predHome === predAway && outcomeHome === outcomeAway) {
-          p += 3;
-          if (predHome === outcomeHome) p += 4;
-        } else {
-          if (predHome === outcomeHome) p += 1;
-          if (predAway === outcomeAway) p += 1;
-          if ((predHome - predAway) * (outcomeHome - outcomeAway) > 0) p += 3;
-          if (p === 5) p += 2;
+        if (match) {
+          if (predHome === predAway && outcomeHome === outcomeAway) {
+            p += 3;
+            if (predHome === outcomeHome) p += 4;
+          } else {
+            if (predHome === outcomeHome) p += 1;
+            if (predAway === outcomeAway) p += 1;
+            if ((predHome - predAway) * (outcomeHome - outcomeAway) > 0) p += 3;
+            if (p === 5) p += 2;
+          }
         }
-      } else {
-        p = prediction.points >= 0 ? prediction.points : 0;
       }
+
       totalScore += p;
       db.collection("users")
         .doc(u.id)
         .collection("predictions")
         .doc(prediction.id)
-        .set({ points: -1 }, { merge: true });
+        .set({ points: p }, { merge: true });
     });
     console.log(`${u.username}: ${totalScore}p`);
-    db.collection("users").doc(u.id).set({ points: 0 }, { merge: true });
+    db.collection("users")
+      .doc(u.id)
+      .set({ points: totalScore }, { merge: true });
   });
 };
